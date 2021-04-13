2def20171860e7fb4b83fc557f92cd67f62d11ef
Gabriel Marques <marques18@gmail.com>
2021-04-13 14:23:05 -0300

    Login UI"

--- app/src/main/java/br/com/everis/sovamu/feature/login/model/LoginUI.kt
+++ app/src/main/java/br/com/everis/sovamu/feature/login/model/LoginUI.kt
@@ -3,5 +3,5 @@ package br.com.everis.sovamu.feature.login.model
 data class LoginUI(
         val email: String,
         val senha: String,
-        val msgError: String
+        var msgError: String
 )
--- app/src/main/java/br/com/everis/sovamu/feature/login/ui/LoginActivity.kt
+++ app/src/main/java/br/com/everis/sovamu/feature/login/ui/LoginActivity.kt
@@ -4,11 +4,12 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import br.com.everis.sovamu.databinding.ActivityMainBinding
+import br.com.everis.sovamu.feature.login.model.LoginUI
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 private const val TAG = "LoginActivity"
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity(), ILoginUI {
 
     private val mainViewModel by viewModel<LoginViewModel>()
     private lateinit var binding: ActivityMainBinding
@@ -18,10 +19,12 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        observeData()
+        binding.button.setOnClickListener {
+            observeData(binding.login)
+        }
     }
 
-    private fun observeData() {
+    override fun observeData(data: LoginUI) {
         mainViewModel.getUserData()
 
         mainViewModel.actionView.observe(this, { state ->
@@ -30,12 +33,20 @@ class LoginActivity : AppCompatActivity() {
                     Log.d(TAG, "Name: ${state.data.name}, Email: ${state.data.email}")
                 }
                 is LoginViewAction.Loading -> {
-
+                    showLoading(state.loading)
                 }
                 is LoginViewAction.Error -> {
-
+                    showError(state.message)
                 }
             }
         })
+    }
+
+    override fun showLoading(loading: Boolean) {
+        TODO("Not yet implemented")
+    }
+
+    override fun showError(msgError: String) {
+        binding.login.msgError = msgError
     }
 }
