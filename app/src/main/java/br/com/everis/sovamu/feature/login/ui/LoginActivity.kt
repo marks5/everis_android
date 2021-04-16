5e3683f9914b7e7feac1071d06b94dc66c21339b
Gabriel Marques <marques18@gmail.com>
2021-04-16 18:39:42 -0300

    Added startActivityHome

--- app/src/main/java/br/com/everis/sovamu/feature/login/ui/LoginActivity.kt
+++ app/src/main/java/br/com/everis/sovamu/feature/login/ui/LoginActivity.kt
@@ -1,5 +1,6 @@
 package br.com.everis.sovamu.feature.login.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -8,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import br.com.everis.sovamu.R
 import br.com.everis.sovamu.databinding.ActivityMainBinding
+import br.com.everis.sovamu.feature.home.ui.HomeActivity
 import br.com.everis.sovamu.feature.login.binding.SoVamuTextWatcher
 import br.com.everis.sovamu.feature.login.model.LoginUI
 import org.koin.android.ext.android.bind
@@ -30,8 +32,18 @@ class LoginActivity : AppCompatActivity() {
 
         observeData()
 
-        binding.edtEmail.addTextChangedListener(SoVamuTextWatcher(viewModel = mainViewModel, SoVamuTextWatcher.Identifier.EMAIL))
-        binding.edtPassword.addTextChangedListener(SoVamuTextWatcher(viewModel = mainViewModel, SoVamuTextWatcher.Identifier.PASSWORD))
+        binding.edtEmail.addTextChangedListener(
+            SoVamuTextWatcher(
+                viewModel = mainViewModel,
+                SoVamuTextWatcher.Identifier.EMAIL
+            )
+        )
+        binding.edtPassword.addTextChangedListener(
+            SoVamuTextWatcher(
+                viewModel = mainViewModel,
+                SoVamuTextWatcher.Identifier.PASSWORD
+            )
+        )
     }
 
     private fun observeData() {
@@ -39,6 +51,7 @@ class LoginActivity : AppCompatActivity() {
             when (state) {
                 is LoginViewAction.Success -> {
                     Timber.tag(TAG).d("Name: ${state.data.name}, Email: ${state.data.email}")
+                    startHome()
                 }
                 is LoginViewAction.Loading -> {
                     Timber.tag(TAG).d("${state.loading}")
@@ -50,9 +63,18 @@ class LoginActivity : AppCompatActivity() {
         })
     }
 
+    private fun startHome() {
+        startActivity(
+            Intent(this, HomeActivity::class.java)
+                .apply {
+                    flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                }
+        )
+    }
+
     private fun enableBtnEntrar(loading: Boolean) {
         binding.btnEntrar.isEnabled = !loading
-        if(loading) {
+        if (loading) {
             binding.btnEntrar.text = getString(R.string.txt_carregando)
         } else {
             binding.btnEntrar.text = getString(R.string.btn_entrar)
